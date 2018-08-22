73699c09e96121a0ddf693aa1f8cb209ae99f8d7
Lucas Diodati <32825590+LucasDiodati@users.noreply.github.com>
2018-08-22 14:46:28 -0300

    Update Ejercicio6.java

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,5 +1,10 @@
 package ejercicio6;
 import java.util.Scanner;
+/*Ingresar 3 valores en 3 variables X,Y y Z. Se desea obtener una rotación de sus valores, 
+es decir que el contenido de Z pase a X, el contenido de X pase a Y, y el contenido de Y pase a Z.
+Se debe mostrar las variables X, Y y Z con sus valores originales y mostrar X, Y y Z con los valores luego de la rotación.
+*/
+
 public class Ejercicio6 {
     public static void main(String[] args) {
             Scanner entrada = new Scanner(System.in);
