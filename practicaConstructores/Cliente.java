83fdca7a7f7ffb8527822facdfe154afe9cbde1f
Lucas Diodati <lucasdiodati@outlook.com>
2018-08-30 11:41:29 -0300

    Add files via upload

--- practicaConstructores/Cliente.java
+++ practicaConstructores/Cliente.java
@@ -5,6 +5,34 @@ public class Cliente {
     private String nombre;
     private Fecha alta;
     private int edad;
+    private String contraseña;
+    private boolean valido;
+    
+    // metodo que valida el largo de contraseña
+    public void validar(boolean vale){
+    int largo = this.contraseña.length();
+        if(largo >= 8){
+        vale = true;
+        System.out.println("La contraseña es valida");
+        }else{
+        System.out.println("La contraseña es invalida");
+        vale = false;
+        }
+    }
+    
+    // Constructor que recibe el valor de contraseña instanciado y llama al metodo de validacion
+    public Cliente(int cdg, String nm, int ant, String contr){
+    this.codigoCliente = cdg;
+    this.nombre = nm;
+    this.edad = ant;
+    this.contraseña = contr;
+    validar(this.valido);
+    }
+    
+    
+    
+    public Cliente(){}
+    
     
     public void setCodigoCliente(int cod){
     this.codigoCliente = cod;
